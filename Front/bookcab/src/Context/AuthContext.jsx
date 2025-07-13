babaa4e5d560579cfebb640e14f5b8daab5fb732
Hemant Dixit <hemantdixit00000@gmail.com>
2025-07-13 16:43:59 +0530

    New

--- Front/bookcab/src/Context/AuthContext.jsx
+++ Front/bookcab/src/Context/AuthContext.jsx
@@ -23,7 +23,7 @@ export const AuthProvider = ({ children }) => {
 
 
   return (
-    <AuthContext.Provider value={{ user, login, logout, sendData, }}>
+    <AuthContext.Provider value={{ user, login, logout, sendData}}>
       {children}
     </AuthContext.Provider>
   );
