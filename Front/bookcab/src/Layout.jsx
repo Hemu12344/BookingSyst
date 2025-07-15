de6ab7b0e7d9c10c0ab804c050b7a2a34ebd84b9
Hemant Dixit <hemantdixit00000@gmail.com>
2025-07-15 21:31:22 +0530

    New one

--- Front/bookcab/src/Layout.jsx
+++ Front/bookcab/src/Layout.jsx
@@ -13,14 +13,16 @@ export const AuthProvider = ({ children }) => {
   const logout = () => localStorage.clear('token');
   const BACKEND = import.meta.env.VITE_BACKEND_URL || "/api";
 
+const path = require('path');
 
-  const path = require('path');
+// Serve static files from Vite frontend build
+app.use(express.static(path.join(__dirname, '..', 'Front', 'bookcab', 'dist')));
 
-  app.use(express.static(path.join(__dirname, '..','Front', 'dist')));
+// React Router fallback
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'Front', 'bookcab', 'dist', 'index.html'));
+});
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..','Front','dist', 'index.html'));
-  });
 
   const sendData = async (data) => {
     setLoad(true)
