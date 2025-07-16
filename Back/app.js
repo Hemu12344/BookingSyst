bb79fbed4a90dd3f977923cafbd8c1c63386e05b
Hemant Dixit <hemantdixit00000@gmail.com>
2025-07-16 07:31:58 +0530

    All clear

--- Back/app.js
+++ Back/app.js
@@ -12,9 +12,19 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const KEY = process.env.API_KEY;
 
-// CORS
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://bookmycab.onrender.com'
+];
+
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
--- Front/bookcab/vite.config.js
+++ Front/bookcab/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path'; // ✅ still useful for alias
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
@@ -10,7 +11,7 @@ export default defineConfig(({ mode }) => {
 
   return {
     base: '/',
-    plugins: [react()], // ✅ no tailwindcss here
+    plugins: [react(),tailwindcss()], // ✅ no tailwindcss here
     build: {
       outDir: 'dist',
     },
